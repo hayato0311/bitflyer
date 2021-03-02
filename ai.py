845fc6a7cc17cb78a0ccf9b29d3dffa1c3ffc37a
Hayato Sato <hayato8088@gmail.com>
2021-03-03 01:00:53 +0900

    first commit

---
+++ ai.py
@@ -0,0 +1,399 @@
+import pandas as pd
+from pathlib import Path
+
+
+from bitflyer_api import *
+
+CHILD_ORDERS_DIR = 'child_orders'
+
+
+class AI:
+    """自動売買システムのアルゴリズム
+
+    """
+
+    def __init__(self, latest_summary, product_code='ETH_JPY', prices={}, small_size=0.01, middle_size=0.1, large_size=0.3, time_diff=9, region='Asia/Tokyo'):
+
+        self.product_code = product_code
+
+        p_child_orders_dir = Path(CHILD_ORDERS_DIR)
+        p_child_orders_dir = p_child_orders_dir.joinpath(product_code)
+        self.product_code = product_code
+
+        self.p_child_orders_path = {
+            'long': p_child_orders_dir.joinpath('long_term.csv'),
+            'short': p_child_orders_dir.joinpath('short_term.csv')
+        }
+        self.child_orders = {
+            'long': pd.DataFrame(),
+            'short': pd.DataFrame()
+        }
+
+        self.latest_summary = latest_summary
+
+        for term in ['long', 'short']:
+            if self.p_child_orders_path[term].exists():
+                self.child_orders[term] = pd.read_csv(
+                    str(self.p_child_orders_path[term]), index_col=0)
+                self.child_orders[term]['child_order_date'] = pd.to_datetime(
+                    self.child_orders[term]['child_order_date'])
+                self.child_orders[term]['child_order_date'] = self.child_orders[term]['child_order_date'].dt.tz_convert(
+                    region)
+
+        self.datetime = datetime.datetime.now(
+            datetime.timezone(datetime.timedelta(hours=9))
+        )
+
+        self.datetime_references = {
+            'now': datetime.datetime.now(datetime.timezone(datetime.timedelta(hours=9))),
+        }
+        self.datetime_references['hourly'] = \
+            self.datetime_references['now'] - datetime.timedelta(hours=1)
+        self.datetime_references['daily'] = \
+            self.datetime_references['now'] - datetime.timedelta(days=1)
+        self.datetime_references['weekly'] = \
+            self.datetime_references['now'] - datetime.timedelta(days=7)
+        self.datetime_references['monthly'] = \
+            self.datetime_references['now'] - datetime.timedelta(days=31)
+
+        self.size = {
+            'small': small_size,
+            'middle': middle_size,
+            'large': large_size
+        }
+
+        if self.latest_summary['BUY']['1w']['price']['max'] < self.latest_summary['BUY']['all']['price']['max'] * 0.25:
+            self.small_size = middle_size
+            self.middle_size = large_size
+            self.large_size = large_size
+
+        self.max_buy_prices = {
+            'long': self.latest_summary['BUY']['all']['price']['max'] * 0.7,
+            'short': self.latest_summary['BUY']['all']['price']['max'] * 0.75
+        }
+
+    def load_latest_child_orders(self, term, child_order_cycle, child_order_acceptance_id,
+                                 related_child_order_acceptance_id='no_id'):
+        print(f'child_order_acceptance_id: {child_order_acceptance_id}')
+        # get a child order from api
+        child_orders_tmp = pd.DataFrame()
+        start_time = time.time()
+        while child_orders_tmp.empty:
+            child_orders_tmp = get_child_orders(
+                region='Asia/Tokyo', child_order_acceptance_id=child_order_acceptance_id)
+            if time.time() - start_time > 10:
+                raise ValueError(f'{child_order_acceptance_id} はすでに存在しません。')
+
+        child_orders_tmp['child_order_cycle'] = child_order_cycle
+        child_orders_tmp['related_child_order_acceptance_id'] = related_child_order_acceptance_id
+        if self.child_orders[term].empty:
+            self.child_orders[term] = child_orders_tmp
+        else:
+            self.child_orders[term].loc[child_order_acceptance_id] = child_orders_tmp.loc[child_order_acceptance_id]
+
+        # csvファイルを更新
+        self.child_orders[term].to_csv(str(self.p_child_orders_path[term]))
+        print(f'{str(self.p_child_orders_path[term])} file was updated')
+
+    def update_child_orders(self, term,
+                            child_order_acceptance_id="",
+                            child_order_cycle="",
+                            related_child_order_acceptance_id="no_id"):
+
+        # --------------------------------
+        # 既存の注文における約定状態を更新
+        # --------------------------------
+        for child_order_acceptance_id_tmp in self.child_orders[term].index.tolist():
+            self.load_latest_child_orders(
+                term=term,
+                child_order_cycle=self.child_orders[term].at[child_order_acceptance_id_tmp,
+                                                             'child_order_cycle'],
+                child_order_acceptance_id=child_order_acceptance_id_tmp,
+                related_child_order_acceptance_id=self.child_orders[term].at[child_order_acceptance_id_tmp,
+                                                                             'related_child_order_acceptance_id']
+            )
+        # --------------------------------
+        # related_child_order_acceptance_idを指定して、注文情報を更新
+        # --------------------------------
+        if not child_order_acceptance_id == "":
+            if child_order_cycle == "":
+                raise ValueError("child_order_cycle must be setted")
+            self.load_latest_child_orders(
+                term=term,
+                child_order_cycle=child_order_cycle,
+                child_order_acceptance_id=child_order_acceptance_id,
+                related_child_order_acceptance_id=related_child_order_acceptance_id
+            )
+
+    def cancel(self, term, child_order_cycle, child_order_acceptance_id):
+        # ----------------------------------------------------------------
+        # キャンセル処理
+        # ----------------------------------------------------------------
+        response = cancel_child_order(product_code=self.product_code,
+                                      child_order_acceptance_id=child_order_acceptance_id)
+        if response.status_code == 200:
+            print('================================================================')
+            print(
+                f'{term}_term {child_order_cycle} order was successfully canceled!!!')
+            print(
+                f'child_order_acceptance_id: {child_order_acceptance_id}')
+            print('================================================================')
+            self.child_orders[term].drop(
+                index=[child_order_acceptance_id],
+                inplace=True
+            )
+            self.update_child_orders(term=term)
+        else:
+            raise Exception("Cancel of buying order was failed")
+
+    def buy(self, term, size, child_order_cycle, reference_price, rate):
+        price = int(reference_price * rate)
+        if price >= self.max_buy_prices[term]:
+            print(f'[{term}, {child_order_cycle}] 過去最高価格に近いため、購入できません。')
+            return
+
+        target_buy_history = pd.DataFrame()
+        same_category_order = pd.DataFrame()
+        target_datetime = self.datetime_references[child_order_cycle]
+        if not self.child_orders[term].empty:
+            target_buy_history = self.child_orders[term].query(
+                'child_order_date > @target_datetime')
+            same_category_order = self.child_orders[term].query(
+                'child_order_state == "ACTIVE" and child_order_cycle == @child_order_cycle').copy()
+
+        if target_buy_history.empty or same_category_order.empty:
+            # ----------------------------------------------------------------
+            # 同じカテゴリーの注文がすでに存在していた場合、前の注文をキャンセルする。
+            # ----------------------------------------------------------------
+            if not same_category_order.empty:
+                self.cancel(
+                    term=term,
+                    child_order_cycle=child_order_cycle,
+                    target_child_order_acceptance_id=same_category_order.index[0]
+                )
+            # ----------------------------------------------------------------
+            # 買い注文
+            # ----------------------------------------------------------------
+
+            response = send_child_order(self.product_code, 'LIMIT', 'BUY',
+                                        price=price, size=size)
+            if response.status_code == 200:
+                print('================================================================')
+                print('買い注文に成功しました!!')
+                print(f'product_code     : {self.product_code}')
+                print(f'price            : {price}')
+                print(f'size             : {size}')
+                print(f'term             : {term}')
+                print(f'child_order_cycle: {child_order_cycle}')
+                print('================================================================')
+                response_json = response.json()
+                self.update_child_orders(
+                    term=term,
+                    child_order_acceptance_id=response_json['child_order_acceptance_id'],
+                    child_order_cycle=child_order_cycle
+                )
+            else:
+                raise Exception(
+                    f"{term}_term {child_order_cycle} buying order was failed")
+
+    def sell(self, term, size, child_order_cycle, rate):
+        related_buy_order = self.child_orders[term].query(
+            'side=="BUY" and child_order_state == "COMPLETED" and child_order_cycle == @child_order_cycle and related_child_order_acceptance_id == "no_id"').copy()
+        if related_buy_order.empty:
+            print(f'[{term}, {child_order_cycle}] 約定済みの買い注文がないため、売り注文はできません。')
+        else:
+            if len(related_buy_order) >= 2:
+                raise ValueError(
+                    '同じフラグを持つ約定済みの買い注文が2つあります。')
+
+            price = int(int(related_buy_order['price']) * rate)
+            response = send_child_order(self.product_code, 'LIMIT', 'SELL',
+                                        price=price, size=size)
+            if response.status_code == 200:
+                print('================================================================')
+                print(
+                    f'売り注文に成功しました！！ 利益: {int(int(related_buy_order["price"]) * (rate-1)) * size}円')
+                print(
+                    f'product_code: {self.product_code} price: {price} size: {size} term: {term}')
+                print('================================================================')
+                response_json = response.json()
+                self.load_latest_child_orders()
+                self.update_child_orders(
+                    term=term,
+                    child_order_cycle=child_order_cycle,
+                    related_child_order_acceptance_id=response_json['child_order_acceptance_id'],
+                    related_buy_order=related_buy_order
+                )
+            else:
+                raise Exception(
+                    f"{term}_term {child_order_cycle} selling order was failed")
+
+    def long_term(self):
+        """
+        [買い注文]
+        絶対条件: 過去最高額の70%範囲内では購入しない。
+        条件1: long_termとして、１日間で1回も買っていない、またはlong_daily_activeがない場合、１日での最安値の80%の価格で指値注文を入れる。(10000円以上)
+        条件2: long_termとして、１週間で1回も買っていない、またはlong_weekly_activeがない場合、１週間での最安値の70%の価格で指値注文を入れる。(10000円以上)
+        条件3: long_termとして、１ヶ月間で1回も買っていない、またはlong_monthly_activeがない場合、１ヶ月間での最安値の75%の価格で指値注文を入れる。(10000円以上)
+
+        ただし、前回の指値注文がACTIVEな場合はその注文を取り消す。
+        """
+        # 最新情報を取得
+        self.update_child_orders(term='long')
+
+        # =================================================================
+        # 条件1:
+        #   - 周期:daily
+        #   - 指値注文価格: １日の最安値の80%
+        #   - サイズ: middle
+        # =================================================================
+        self.buy(
+            term='long',
+            size=self.size['middle'],
+            child_order_cycle='daily',
+            reference_price=self.latest_summary['BUY']['1d']['price']['min'],
+            rate=0.80
+        )
+
+        # =================================================================
+        # 条件2:
+        #   - 周期: weekly
+        #   - 指値注文価格: １週間の最安値の70%
+        #   - サイズ: large
+        # =================================================================
+        self.buy(
+            term='long',
+            size=self.size['large'],
+            child_order_cycle='weekly',
+            reference_price=self.latest_summary['BUY']['1w']['price']['min'],
+            rate=0.70
+        )
+
+        # =================================================================
+        # 条件3:
+        #   - 周期: monthly
+        #   - 指値注文価格: １ヶ月間の最安値の75%
+        #   - サイズ: middle
+        # =================================================================
+        self.buy(
+            term='long',
+            size=self.size['middle'],
+            child_order_cycle='monthly',
+            reference_price=self.latest_summary['BUY']['1m']['price']['min'],
+            rate=0.75
+        )
+
+    def short_term(self):
+        """
+        [買い注文]
+        絶対条件: 過去最高額の75%範囲内では購入しない。
+        条件1: short_termとして、1時間で1回も買っていない、またはshort_hourly_buy_activeがない場合、1時間での最安値の90%の価格で指値注文を入れる。(10000円以上)
+        条件2: short_termとして、１日間で1回も買っていない、またはshort_daily_buy_activeがない場合、１日での最安値の95%の価格で指値注文を入れる。(1000円単位)
+        条件3: short_termとして、１週間で1回も買っていない、またはshort_weekly_buy_activeがない場合、１週間での最安値の85%の価格で指値注文を入れる。(1000円単位)
+
+        ただし、前回の指値注文がACTIVEな場合はその注文を取り消す。
+
+        [売り注文]
+        条件1: short_hourly_buy_complitedがあり、related_child_order_acceptance_idが'no_id'の場合、その指値注文の110%の価格で指値注文を入れる。(10000円以上)
+        条件2: short_daily_buy_complitedがあり、related_child_order_acceptance_idが'no_id'の場合、その指値注文の105%上の価格で指値注文を入れる。(1000円単位)
+        条件3: short_weekly_buy_complitedがあり、related_child_order_acceptance_idが'no_id'の場合、その指値注文の115%の価格で指値注文を入れる。(1000円単位)
+
+
+        """
+
+        # 最新情報を取得
+        self.update_child_orders(term='short')
+
+        # =================================================================
+        # 買い注文
+        # =================================================================
+
+        # =================================================================
+        # 条件1:
+        #   - 周期:hourly
+        #   - 指値注文価格: 1時間の最安値の90%
+        #   - サイズ: small
+        # =================================================================
+        self.buy(
+            term='short',
+            child_order_cycle='hourly',
+            size=self.size['small'],
+            reference_price=self.latest_summary['BUY']['1h']['price']['min'],
+            rate=0.90
+        )
+
+        # =================================================================
+        # 条件2:
+        #   - 周期:daily
+        #   - 指値注文価格: １日の最安値の95%
+        #   - サイズ: small
+        # =================================================================
+        self.buy(
+            term='short',
+            child_order_cycle='daily',
+            size=self.size['small'],
+            reference_price=self.latest_summary['BUY']['1d']['price']['min'],
+            rate=0.95
+        )
+
+        # =================================================================
+        # 条件3:
+        #   - 周期:weekly
+        #   - 指値注文価格: １週間の最安値の85%
+        #   - サイズ: small
+        # =================================================================
+        self.buy(
+            term='short',
+            child_order_cycle='weekly',
+            size=self.size['small'],
+            reference_price=self.latest_summary['BUY']['1w']['price']['min'],
+            rate=0.85
+        )
+
+        # =================================================================
+        # 売り注文
+        # =================================================================
+
+        if self.child_orders['short'].empty:
+            print(f'[short] 買い注文がないため、売り注文はできません。')
+            return
+
+        # =================================================================
+        # 条件1:
+        #   - 周期:hourly
+        #   - 指値注文価格: 購入価格の120%
+        #   - サイズ: small
+        # =================================================================
+        self.sell(
+            term='short',
+            size=self.size['small'],
+            child_order_cycle='hourly',
+            rate=1.20
+        )
+
+        # =================================================================
+        # 条件2:
+        #   - 周期:daily
+        #   - 指値注文価格: 購入価格の115%
+        #   - サイズ: small
+        # =================================================================
+        self.sell(
+            term='short',
+            size=self.size['small'],
+            child_order_cycle='daily',
+            rate=1.15
+        )
+
+        # =================================================================
+        # 条件3:
+        #   - 周期:weekly
+        #   - 指値注文価格: 購入価格の130%
+        #   - サイズ: small
+        # =================================================================
+
+        self.sell(
+            term='short',
+            size=self.size['small'],
+            child_order_cycle='weekly',
+            rate=1.30
+        )
---
+++ bitflyer_api.py
@@ -0,0 +1,291 @@
+import hashlib
+import hmac
+import requests
+import time
+import pytz
+import datetime
+import json
+from pprint import pprint
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+# HTTP Public API (GET)
+HTTP_PUBLIC_API = {
+    'GET': {
+        'market_list': '/v1/getmarkets',
+        'ticker': '/v1/getticker',
+        'executions': '/v1/getexecutions',
+    }
+}
+
+# HTTP Private API
+
+HTTP_PRIVATE_API = {
+    'GET': {
+        'withdraw_history': '/v1/me/getwithdrawals',
+        'balance': '/v1/me/getbalance',
+        'coin_in_history': '/v1/me/getcoinins',
+        'coin_out_history': '/v1/me/getcoinins',
+        'child_orders': '/v1/me/getchildorders'
+
+    },
+    'POST': {
+        'withdraw': '/v1/withdraw',
+        'send_child_order': '/v1/me/sendchildorder',
+        'cancel_child_order': '/v1/me/cancelchildorder',
+        'cancel_all_child_order': '/v1/me/cancelallchildorders'
+    }
+
+}
+
+API_KEY_PATH = 'private/api_key.json'
+
+
+class BitflyerAPI:
+    def __init__(self, method, process_path, params={}):
+        self.base_url = 'https://api.bitflyer.com'
+        self.process_path = process_path
+        self.params = params
+
+        self.query = ''
+
+        if len(params) > 0:
+            self.query += '?'
+            for i, (key, val) in enumerate(params.items()):
+                if i != len(params) - 1:
+                    self.query += f'{key}={val}&'
+                else:
+                    self.query += f'{key}={val}'
+
+        self.process_path_with_query = process_path + self.query
+
+        self.api_url = self.base_url + self.process_path
+
+        self.method = method
+        self.unix_time = str(time.time())
+
+        with open(API_KEY_PATH) as f:
+            id = json.load(f)
+        self.api_key = id['api_key']
+        self.api_secret = id['api_secret']
+
+    def sign(self, body={}):
+        if self.method == 'GET':
+            payload = self.unix_time + self.method + self.process_path_with_query
+        elif self.method == 'POST':
+            payload = self.unix_time + self.method + \
+                self.process_path + json.dumps(body)
+        else:
+            raise ValueError('Method name is not correct. set "GET" or "POST"')
+
+        return hmac.new(self.api_secret.encode('utf-8'), payload.encode('utf-8'),
+                        hashlib.sha256).hexdigest()
+
+    def get(self, private=True, name=''):
+        if private:
+            headers = {
+                'ACCESS-KEY': self.api_key,
+                'ACCESS-TIMESTAMP': self.unix_time,
+                'ACCESS-SIGN': self.sign(),
+                'Content-Type': 'application/json'
+            }
+        else:
+            headers = {'Content-Type': 'application/json'}
+
+        result = requests.get(
+            self.api_url, headers=headers, params=self.params)
+
+        if result.status_code == 200:
+            print(f'{name} was successful!')
+        else:
+            print(f'{name} was Failed')
+            pprint(result.json())
+        return result
+
+    def post(self, body, name=''):
+        headers = {
+            'ACCESS-KEY': self.api_key,
+            'ACCESS-TIMESTAMP': self.unix_time,
+            'ACCESS-SIGN': self.sign(body),
+            'Content-Type': 'application/json'
+        }
+        result = requests.post(self.api_url, headers=headers, json=body)
+        if result.status_code == 200:
+            print(f'{name} was successful!')
+        else:
+            print(f'{name} was Failed')
+            pprint(result.json())
+        return result
+
+
+# HTTP_PUBLIC_API
+
+
+def get_ticker(product_code='ETH_JPY'):
+    """プロダクトの情報を取得
+    Args:
+        product_code (str, optional): 'BTC_JPY', 'ETH_BTC', 'BCH_BTC', 'ETH_JPY'が利用可能。デフォルト値は 'ETH_JPY'。
+    Returns:
+        dict: レスポンス
+    """
+    method = 'GET'
+    process_path = HTTP_PUBLIC_API[method]['ticker']
+    params = {'product_code': product_code}
+
+    bf = BitflyerAPI(method, process_path, params=params)
+    result = bf.get(private=False)
+
+    return result
+
+
+def get_executions(product_code='ETH_JPY', count=100, before=0, after=0, region='Asia/Tokyo'):
+    """約定履歴を取得
+
+    Args:
+        product_code (str, optional): 'BTC_JPY', 'ETH_BTC', 'BCH_BTC', 'ETH_JPY'が利用可能。デフォルト値は 'ETH_JPY'。
+        count (int, optional):  結果の個数を指定。デフォルトは100。
+        before (str, optional): このパラメータに指定した値より小さい id を持つデータを取得。
+        after (str, optional): このパラメータに指定した値より大きい id を持つデータを取得。
+        region (str, optional): 住んでいる地域。
+
+    Returns:
+        [type]: [description]
+    """
+    method = 'GET'
+    process_path = HTTP_PUBLIC_API[method]['executions']
+    params = {'product_code': product_code, 'count': count}
+
+    if before != 0:
+        params['before'] = before
+    if after != 0:
+        params['after'] = after
+
+    bf = BitflyerAPI(method, process_path, params=params)
+    result = bf.get(private=False, name='get_executions')
+    if result.status_code == 200:
+        df_result = pd.DataFrame(result.json())
+        if not df_result.empty:
+            df_result['exec_date'] = pd.to_datetime(
+                df_result['exec_date'], utc=True)
+            df_result = df_result.set_index('exec_date', drop=True)
+            df_result = df_result.tz_convert(region)
+
+        return df_result
+    else:
+        raise Exception('error')
+
+
+# HTTP_PRIVATE_API
+
+
+def get_balance():
+    method = 'GET'
+    process_path = HTTP_PRIVATE_API[method]['balance']
+
+    bf = BitflyerAPI(method, process_path)
+
+    result = bf.get(private=True, name='get_balance')
+
+    df = pd.DataFrame(result.json())
+
+    return df
+
+
+def get_child_orders(product_code='ETH_JPY', count=100, before=0, after=0,
+                     child_order_state='',
+                     child_order_id='', child_order_acceptance_id='',
+                     parent_order_id='', region='Asia/Tokyo'):
+
+    method = 'GET'
+    process_path = HTTP_PRIVATE_API[method]['child_orders']
+    params = {'product_code': product_code, 'count': count}
+
+    if before != 0:
+        params['before'] = before
+    if after != 0:
+        params['after'] = after
+    if child_order_state != '':
+        params['child_order_state'] = child_order_state
+    if child_order_id != '':
+        params['child_order_id'] = child_order_id
+    if child_order_acceptance_id != '':
+        params['child_order_acceptance_id'] = child_order_acceptance_id
+    if parent_order_id != '':
+        params['parent_order_id'] = parent_order_id
+
+    bf = BitflyerAPI(method, process_path, params=params)
+
+    response = bf.get(private=True, name='get_child_orders')
+    df = pd.DataFrame()
+    if response.status_code == 200:
+        df = pd.DataFrame(response.json())
+        if not df.empty:
+            df['child_order_date'] = pd.to_datetime(
+                df['child_order_date'], utc=True)
+            df['child_order_date'] = df['child_order_date'].dt.tz_convert(
+                region)
+            df = df.sort_values('child_order_date').reset_index(drop=True)
+            df = df.set_index('child_order_acceptance_id')
+
+    return df
+
+
+def send_child_order(product_code, child_order_type, side, price, size,
+                     minute_to_expire=43200, time_in_force='GTC'):
+    """新規注文を出す
+    Args:
+        product_code (str): 注文するプロダクト。BTC_JPY, ETH_BTC, BCH_BTC, ETH_JPYが利用可能
+        child_order_type (str): 注文タイプ。指値注文（値段を指定して購入）の場合は "LIMIT", 成行注文（現在の価格で購入）の場合は "MARKET" を指定
+        side (str): 買い注文の場合は "BUY", 売り注文の場合は "SELL" を指定
+        price (int): 価格を指定。指値注文（LIMIT）の場合に用いる。
+        size (int): 注文数量を指定。
+        minute_to_expire (int, optional): 期限切れまでの時間を分で指定。デフォルトは43200(30日間)
+        time_in_force (str, optional): 執行数量条件 を "GTC", "IOC", "FOK" のいずれかで指定。デフォルトは 'GTC'.
+
+    Returns:
+        dict: レスポンス
+    """
+    method = 'POST'
+    process_path = HTTP_PRIVATE_API[method]['send_child_order']
+
+    body = {
+        "product_code": product_code,
+        "child_order_type": child_order_type,
+        "side": side,
+        "price": price,
+        "size": size,
+        "minute_to_expire": minute_to_expire,
+        "time_in_force": time_in_force
+    }
+
+    bf = BitflyerAPI(method, process_path)
+
+    result = bf.post(body, name='send_child_order')
+
+    return result
+
+
+def cancel_child_order(product_code, child_order_acceptance_id):
+    """注文をキャンセルする
+    Args:
+        product_code (str): 注文するプロダクト。BTC_JPY, ETH_BTC, BCH_BTC, ETH_JPYが利用可能
+        child_order_id (str): キャンセルする注文の ID
+
+    Returns:
+        dict: レスポンス
+    """
+    method = 'POST'
+    process_path = HTTP_PRIVATE_API[method]['cancel_child_order']
+
+    body = {
+        "product_code": product_code,
+        'child_order_acceptance_id': child_order_acceptance_id
+    }
+
+    bf = BitflyerAPI(method, process_path)
+
+    result = bf.post(body, name='cancel_child_order')
+
+    return result
